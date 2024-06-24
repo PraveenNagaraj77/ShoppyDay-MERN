9464d2c61880bd0f4da8ea26046394c65ceca3a0
PraveenNagaraj77 <praveennagaraj76@gmail.com>
2024-06-24 14:12:02 +0530

    Latest Commit

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
-const app = express();
+const port = 4000;
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
